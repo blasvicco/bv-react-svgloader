7ecb62f78264c8fcdfa2b623645bc40df63dd6f3
blasvicco@gmail.com <blasvicco@gmail.com>
2022-03-14 19:57:22 +0100

    Improve: Making it browser compatible.

--- svgloader.js
+++ svgloader.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-export default class SvgLoader extends Component {
+export default class SvgLoader extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
